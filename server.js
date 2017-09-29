901378717734563e540e59abb3378d30302e2e6f
rick22 Alcazar <rickity_rick_22@hotmail.com>
2017-09-29 02:13:55 -0700

    Final Commit

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ var methodOverride = require('method-override');
 
 var app = express();
 
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + '/public'));
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-
+// override with POST having ?_method=DELETE
 app.use(methodOverride('_method'));
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({
@@ -17,10 +18,8 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
-var routes = require('./controllers/controller.js');
+var routes = require('./controllers/burgers_controller.js');
 app.use('/', routes);
 
-var PORT = process.env.PORT || 3306;
-app.listen(PORT, function() {
-    console.log('find me at PORT: ' + PORT);
-})
+var port = process.env.PORT || 3000;
+app.listen(port);
